2ea3380e1c55b2e18ea5172ff9f7e23f4e3feaf8
kyo <dlrywns91@Naver.com>
2017-09-27 16:55:43 +0900

    no message

---
+++ MyApplication/app/src/main/java/com/example/kyo/gasstation/CardIF.java
@@ -0,0 +1,13 @@
+package com.example.kyo.gasstation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CardIF extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_if);
+    }
+}
---
+++ MyApplication/app/src/main/java/com/example/kyo/gasstation/LocalGS.java
@@ -0,0 +1,13 @@
+package com.example.kyo.gasstation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LocalGS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_local_gs);
+    }
+}
